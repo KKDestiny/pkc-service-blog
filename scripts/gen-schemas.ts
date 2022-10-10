954b356d6748168095d4164921caf686a73bff79
linxiaozhou <1139904786@qq.com>
2022-10-10 21:55:36 +0800

    feature: 优化schema生成脚本

--- scripts/gen-schemas.ts
+++ scripts/gen-schemas.ts
@@ -2,11 +2,28 @@
  * @Author: linxiaozhou.com
  * @LastEditors: linxiaozhou.com
  * @Description: 导出项目swagger数据
- */ import { run } from "../src/specs/tools/schemas.tool";
+ */
+import { run } from "../src/specs/tools/schemas.tool";
+
+import { schema as articlesSchema } from "../src/models/articles.model";
+import { schema as categoriesSchema } from "../src/models/categories.model";
+
+const schemas = [
+  {
+    filename: "articles",
+    schemaName: "Articles",
+    schema: articlesSchema,
+  },
+  {
+    filename: "categories",
+    schemaName: "Categories",
+    schema: categoriesSchema,
+  },
+];
 
 async function exec() {
   console.log(`===================== Begin Generate Schemas =====================`);
-  await run();
+  await run(schemas);
   console.log(`====================== End Generate Schemas ======================\n\n`);
 }
 exec();
